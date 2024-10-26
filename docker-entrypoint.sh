9677ee9d5c4e2b7bf65c31d1cafa24f17cc68dd9
TYChen1102 <chentaiyang1102@gmail.com>
2024-10-25 21:42:22 -0400

    dataset updates...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/test_data
-mount --bind /mnt/gcs_data /app/test_data
+mkdir -p /app/smarteats_data
+mount --bind /mnt/gcs_data/smarteats_data /app/smarteats_data
 
 pipenv shell
